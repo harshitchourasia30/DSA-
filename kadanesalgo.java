1d687961f8751088dd0843df6f94fc4ad09ce7cd
Harshit.30 <harshitchourasia3006@gmail.com>
2026-01-11 17:55:02 +0530

    edgecasekadanes

--- kadanesalgo.java
+++ kadanesalgo.java
@@ -23,3 +23,21 @@ public class kadanesalgo {
     }
     
 }
+
+
+/*  Given an integer array nums, find the subarray with the largest sum, and return its sum
+   
+           for cases like  nums = [-1] above algo didnt work work becasue we set cs=0 ,, therefor we use this.
+           
+ class Solution {
+    public int maxSubArray(int[] nums) {
+        int ms = nums[0];
+        int cs = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            cs = Math.max(nums[i], cs + nums[i]);
+            ms = Math.max(ms, cs);
+        }
+        return ms;
+    }
+}                                                            */
